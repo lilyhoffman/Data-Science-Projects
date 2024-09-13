c96c4c88345ea77988b1b57415c2fdc53faee1fc
lilyhoffman <lilynaavhoffman@gmail.com>
2024-09-12 21:19:27 -0400

    images

--- UnsubscriberManager/ano22.py
+++ UnsubscriberManager/ano22.py
@@ -170,7 +170,7 @@ def upload_page():
 
 
 def main():
-    st.sidebar.image(image="ukg.webp", use_column_width=True)
+    st.sidebar.image(image="images/ukg.webp", use_column_width=True)
     st.sidebar.title("Navigation")
     page = st.sidebar.selectbox("Select a page", ["Upload", "About"])
 
