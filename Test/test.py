8b06bfe898460d49cf80c6b49bf5553d7fdb4eff
lilyhoffman <lilynaavhoffman@gmail.com>
2024-09-25 14:53:38 -0400

    df

--- Test/test.py
+++ Test/test.py
@@ -8,7 +8,7 @@ import streamlit as st
 # Function to process the URL
 def process_url(url):
     # service=Service(ChromeDriverManager().install())
-    service = Service('../UnsubscriberManager/images/chromedriver.exe')
+    service = Service('../UnsubscriberManager/images/chromedriver')
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options, service=service)
